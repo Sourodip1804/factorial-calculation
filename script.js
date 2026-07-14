b218417739cfeba05dbf39e8a6d4fe69166ecbac
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-14 13:11:18 +0530

    Adding a new file

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let a = 6
 // defining a function
 
 function factorial(number){
+    //short method to make a array
     let arr = Array.from(Array(number+1).keys()) //this return a array
     console.log(arr.slice(1,))
     // to create for loop using arrow function
