c075e149120db6068ee9e9654adc8d7c90425a71
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-14 14:38:00 +0530

    Adding a new lines

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ function factorial(number){
 
 function facFor(number){
     let fac = 1;
-    
+    for (let i = 0; i <= number; i++) {
+        const element = array[i];
+        
+    }
 }
 
 factorial(a)
