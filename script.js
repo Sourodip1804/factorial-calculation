6802f59b39950ddfe7e56d2fa5eeb6609ad6ed91
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-14 13:07:00 +0530

    Adding a new file

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function factorial(number){
     let arr = Array.from(Array(number+1).keys()) //this return a array
     console.log(arr.slice(1,))
     // to create for loop using arrow function
-    let c = arr.slice(1,).reduce((a,b))
+    let c = arr.slice(1,).reduce((a,b))=>
 }
 factorial(a)
