1e67da91d2507e63be2d22644f557ba496eaf6f6
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-14 13:06:00 +0530

    Adding a new file

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let a = 6
 function factorial(number){
     let arr = Array.from(Array(number+1).keys()) //this return a array
     console.log(arr.slice(1,))
-
+    let c = arr.slice(1,).reduce((a,b))
 }
 factorial(a)
